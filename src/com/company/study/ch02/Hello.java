8d1ea69bf08a36e133a2bbdb0a7ea3a4b4c5f3ea
lasldjf123@naver.com <lasldjf123@naver.com>
2022-07-26 17:34:45 +0900

    1

---
+++ src/com/company/study/ch02/Hello.java
@@ -0,0 +1,47 @@
+package com.company.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+/*
+   데이터타입 변수명 = 값; 
+   데이터타입 변수명
+ * */
+		
+		
+		
+		boolean abc = true;
+		boolean xyz;
+		xyz = false;
+		int km = 100;
+		
+		String Title = "CAT'S EYE DESK MAY";
+		int review = 7;
+		int price = 22000;
+		double sold = 7.4;
+		String detail = "설명";
+		String option = "color";
+		int stock = 0;
+		int price_total = 0;
+		
+		
+		
+		
+		
+		System.out.println("상품이름:"+ Title);
+		System.out.println(review + "개 리뷰");
+		System.out.println(price + "원");
+		System.out.println(review + "sold");
+		System.out.println("상품설명" + detail);
+		System.out.println("상품색삭" + option);
+		System.out.println("총" + stock +"개");
+		System.out.println("총" + price_total + "원");	
+		
+		
+		
+		
+	}
+
+}

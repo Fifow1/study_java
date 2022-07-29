48acf170fbf55adf418847239bb2f6126aa1102d
lasldjf123@naver.com <lasldjf123@naver.com>
2022-07-29 17:19:28 +0900

    1

---
+++ src/com/company/study/ch02/FloatDoubleExample.java
@@ -0,0 +1,17 @@
+package com.company.study.ch02;
+
+public class FloatDoubleExample {
+	
+	public static void main(String[] args) {
+		
+		double var1 = 3.13;
+		double var2 = 3.14f;
+		double var3 = 3.13F;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		
+	}
+
+}
---
+++ src/com/company/study/ch02/IntExample.java
@@ -0,0 +1,23 @@
+package com.company.study.ch02;
+
+public class IntExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int var1 = 10;		//십진수
+		int var2 = 012;		//8진수
+		int var3 = 0xa;		//16진수
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		
+		
+		
+		
+	
+
+	}
+
+}

0974129847b477181ab1fe6ea4f3be07529866b3
lasldjf123@naver.com <lasldjf123@naver.com>
2022-08-01 17:27:03 +0900

    1

---
+++ src/com/company/study/ch02/CastingExample.java
@@ -0,0 +1,63 @@
+package com.company.study.ch02;
+
+public class CastingExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//강제 형변환 : casting : (원하는 형) 변수명 
+		
+		double doubleValue = 1234567;
+		System.out.println("doubleValue:" + doubleValue);
+		
+		double doubleValue2= 1234567891;
+		System.out.println("doubleValue2:"+ doubleValue2);
+		
+		float floatValue = (float) doubleValue;
+		System.out.println("floatValue:" + floatValue);
+		
+		long longValue = (long) floatValue;
+		System.out.println("longValue:" + longValue);
+		
+		int intValue = (int) longValue;
+		System.out.println("intValue:" + intValue);
+		
+		short shortValue = (short) intValue;
+		System.out.println("shortValue:" + shortValue);
+		
+		byte byteValue = (byte) shortValue;
+		System.out.println("byteValue:" + byteValue);
+		
+		
+		String stringValue = String.valueOf(intValue);
+		System.out.println("srtingValue:" + stringValue);
+		
+		int intValue2 = Integer.parseInt(stringValue);
+		System.out.println("intValue2:" + intValue2);
+		
+		
+		
+		// int 를 string 으로 변환
+		
+		int in_1 = 123;
+		String str_1 = Integer.toString(in_1);
+		System.out.println(str_1);
+		
+		
+		
+		// string 을 int 로 변환
+		
+		String str_2 = "123";
+		int in_2 = Integer.parseInt(str_2);
+		System.out.println(in_2);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
--- src/com/company/study/ch02/PromotionExample.java
+++ src/com/company/study/ch02/PromotionExample.java
@@ -10,25 +10,74 @@ public class PromotionExample {
 		// 큰 형이 작은 형으로 변환이 되려면 문제가 생기거나 변환이 이루어져도 데이터 손실이 발생한다.
 		// byte < short < int < long < float < double
 		
+
+		
+//		int intValue = byteValue;
+	//	System.out.println("intValue:" + intValue);
+//----------------------------------------------------------------------------------------
+		
+		
 		byte byteValue =10;
 		System.out.println("byteValue: " + byteValue);
 		
-		int intValue = byteValue;
+		short shortValue = byteValue;
+		System.out.println("shortValue:" + shortValue);
+		
+		
+		int intValue = shortValue;
 		System.out.println("intValue:" + intValue);
-//----------------------------------------------------------------------------------------
 		
-		int intValue2 = 1234567890;
-		System.out.println("intValue:" + intValue);
+		long longValue = intValue;
+		System.out.println("longValue:" + longValue);
 		
-		byte byteValue2 = intValue2;
-		System.out.println("byteValue: " + byteValue);
+		float floatValue = longValue;
+		System.out.println("floatValue:" + floatValue);
+		
+		double doubleValue = floatValue;
+		System.out.println("doublValue:" + doubleValue);
 		
 		
-		int intValue3 = 200;
-		System.out.println("intValue:"+intValue3+"입니다!!");
 		
-		double doubleValue = intValue3;
-		System.out.println("doubleValue:"+ doubleValue +"입니다!!");
+		
+//sysout		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		int intValue2 = 1234567890;
+//		System.out.println("intValue:" + intValue);
+		
+//		byte byteValue2 = intValue2;
+//		System.out.println("byteValue: " + byteValue);
+		
+		
+//		int intValue3 = 200;
+//		System.out.println("intValue:"+intValue3+"입니다!!");
+		
+//		double doubleValue = intValue3;
+//		System.out.println("doubleValue:"+ doubleValue +"입니다!!");
 		
 	}
 

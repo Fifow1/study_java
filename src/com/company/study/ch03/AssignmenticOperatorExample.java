673a181261030f1ae6f187962742205d5af9b9c1
lasldjf123@naver.com <lasldjf123@naver.com>
2022-08-05 17:11:43 +0900

    1

---
+++ src/com/company/study/ch03/AssignmenticOperatorExample.java
@@ -0,0 +1,25 @@
+package com.company.study.ch03;
+
+public class AssignmenticOperatorExample {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		
+		int result = 0 ;
+		int result2 = 0 ;
+		int result3 = 0 ;
+		
+		result += 10;
+		System.out.println(result);
+		
+		
+		result2 -= 10;
+		System.out.println(result2);
+		
+		result3 *= 10;
+		System.out.println(result3);
+	}
+
+}

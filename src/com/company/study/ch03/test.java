2497a8079f58b9747c0bdca3c117f258c1ee3c9e
lasldjf123@naver.com <lasldjf123@naver.com>
2022-08-08 17:33:32 +0900

    1

--- src/com/company/study/ch03/test.java
+++ src/com/company/study/ch03/test.java
@@ -6,14 +6,7 @@ public class test {
 		// TODO Auto-generated method stub
 		
 		
-		double a = (double) 2/3 + 3/3 - 5/8 ; 
-				
-				
-		double b = (double) 4/5 + 45/5;
 		
-		double c = (a)*(b);
-		
-		System.out.println(c);
 		
 		
 		
